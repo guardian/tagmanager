4703f9149283076d4ab934d531bbaac60c3501fd
Christopher Lloyd <christopher.lloyd@theguardian.com>
2015-10-27 16:32:55 +0000

    Case Sensitive Build is Case Sensitive

--- public/reactBootstrap.js
+++ public/reactBootstrap.js
@@ -3,6 +3,6 @@ import { render } from 'react-dom';
 import Router from 'react-router';
 import routes from './routes/routes';
 
-import ReactApp from 'components/reactApp.react';
+import ReactApp from 'components/ReactApp.react';
 
 render(<Router routes={routes} />, document.getElementById('react-mount'));
