4dd7ffbeca9ea3e4727f9a096620652f4da8d2b9
Christopher Lloyd <christopher.lloyd@theguardian.com>
2015-10-29 11:17:49 +0000

    Added error display to application and handling to get request.

--- public/components/ReactApp.react.js
+++ public/components/ReactApp.react.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import Header from './Header.react';
 
-export default class ReactApp extends React.Component {
+class ReactApp extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.renderErrorBar = this.renderErrorBar.bind(this);
+    }
+
+    renderErrorBar() {
+      if (!this.props.error) {
+        return false;
+      }
+
+      return (
+        <div className="error-bar">
+          {this.props.error || 'An error has occured, please refresh your browser. If this problem persists please contact Central Production'}
+        </div>
+      );
     }
 
     render () {
         return (
             <div className="wrapper">
+                {this.renderErrorBar()}
                 <Header />
                 <div className="editor">
                     <h1>React is running.</h1>
@@ -19,3 +34,12 @@ export default class ReactApp extends React.Component {
         );
     }
 }
+
+//REDUX CONNECTIONS
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+  return { error: state.error };
+}
+
+export default connect(mapStateToProps)(ReactApp);
--- public/reducers/rootReducer.js
+++ public/reducers/rootReducer.js
@@ -1,10 +1,14 @@
-import {TAG_GET_RECEIVE} from '../actions/tagActions';
+import {TAG_GET_RECEIVE, TAG_GET_ERROR} from '../actions/tagActions';
 
-export default function(state = {}, action) {
+export default function tag(state = {tag: false}, action) {
   switch (action.type) {
   case TAG_GET_RECEIVE:
     return Object.assign({}, state, {
       tag: action.tag
+    });
+  case TAG_GET_ERROR:
+    return Object.assign({}, state, {
+      error: action.message
     });
   default:
     return state;
