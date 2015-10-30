fd591282667be8155a3bcc99937fe9e7e31dfcfd
Christopher Lloyd <christopher.lloyd@theguardian.com>
2015-10-30 15:20:09 +0000

    webpack now watches with build-js-dev command

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 cd public
-printf "\nCompiling Client JS... \n\r\n\r"
-npm run build-js-dev
-
+npm run build-js-dev &
 cd ..
-printf "\nRunning Application... \n\r\n\r"
 AWS_PROFILE=composer sbt run
