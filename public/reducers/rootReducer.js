32efe42b19f3e346c7d1ae1a46020e37e4973786
Christopher Lloyd <christopher.lloyd@theguardian.com>
2015-10-30 15:34:27 +0000

    Quick save state fix

--- public/reducers/rootReducer.js
+++ public/reducers/rootReducer.js
@@ -1,10 +1,11 @@
 import {TAG_GET_REQUEST, TAG_GET_RECEIVE, TAG_GET_ERROR} from '../actions/getTag';
 import {TAG_UPDATE} from '../actions/updateTag';
-import {TAG_SAVE} from '../actions/saveTag';
+import {TAG_SAVE_REQUEST, TAG_SAVE_RECEIVE, TAG_SAVE_ERROR} from '../actions/saveTag';
 
 const saveState = {
   dirty: 'SAVE_STATE_DIRTY',
   clean: 'SAVE_STATE_CLEAN',
+  inprogress: 'SAVE_STATE_INPROGRESS',
   error: 'SAVE_STATE_ERROR'
 };
 
@@ -14,6 +15,7 @@ export default function tag(state = {
   saveState: undefined
 }, action) {
   switch (action.type) {
+
   case TAG_GET_REQUEST:
     return Object.assign({}, state, {
       tag: false,
@@ -29,15 +31,25 @@ export default function tag(state = {
       error: action.message,
       saveState: undefined
     });
+
   case TAG_UPDATE:
     return Object.assign({}, state, {
       tag: action.tag,
       saveState: saveState.dirty
     });
-  case TAG_SAVE:
+
+  case TAG_SAVE_RECEIVE:
+    return Object.assign({}, state, {
+      saveState: saveState.inprogress
+    });
+  case TAG_SAVE_RECEIVE:
     return Object.assign({}, state, {
       tag: action.tag,
       saveState: saveState.clean
+    });
+  case TAG_SAVE_ERROR:
+    return Object.assign({}, state, {
+      error: action.message
     });
   default:
     return state;
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd public
-npm run build-js-dev &
-cd ..
-AWS_PROFILE=composer sbt run
