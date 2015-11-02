7ce62b3833b6a4ee20e3ed2ab2fec1b4e435e599
Christopher Lloyd <christopher.lloyd@theguardian.com>
2015-11-02 17:21:43 +0000

    Setup basic internal/external/slug linking.

--- public/components/Tag/TagBasicInfo.react.js
+++ public/components/Tag/TagBasicInfo.react.js
@@ -4,19 +4,48 @@ export default class TagBasicInfo extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      externalNameLocked: props.tag.externalName === props.tag.internalName,
+      slugLocked: this.slugify(props.tag.internalName) === props.tag.slug //determine this
+    };
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({
+      externalNameLocked: props.tag.externalName === props.tag.internalName,
+      slugLocked: this.slugify(props.tag.internalName) === props.tag.slug //determine this
+    });
   }
 
   onUpdateInternalName(e) {
     this.props.updateTag(Object.assign({}, this.props.tag, {
-      internalName: e.target.value
+      internalName: e.target.value,
+      externalName: this.state.externalNameLocked ? e.target.value : this.props.tag.externalName,
+      slug: this.state.slugLocked ? this.slugify(e.target.value) : this.props.tag.slug
     }));
   }
 
   onUpdateExternalName(e) {
     this.props.updateTag(Object.assign({}, this.props.tag, {
-      externalName: e.target.value,
-      slug: e.target.value.replace(/[^a-z0-9-]+$/i, '-')
+      externalName: e.target.value
     }));
+  }
+
+  onUpdateSlug(e) {
+    this.props.updateTag(Object.assign({}, this.props.tag, {
+      slug: this.slugify(e.target.value)
+    }));
+  }
+
+  toggleUnlock(name) {
+    var newState = {};
+    newState[name] = !this.state[name];
+    this.setState(newState);
+  }
+
+  slugify(text) {
+    return text.toLowerCase().replace(/[^a-z0-9-]/g, '-');
   }
 
   render () {
@@ -25,16 +54,33 @@ export default class TagBasicInfo extends React.Component {
       return false;
     }
 
+    var classNames = {
+      externalName: {
+        lock: this.state.externalNameLocked ? 'tag-edit__linked-field__lock' : 'tag-edit__linked-field__lock--unlocked',
+        link: this.state.externalNameLocked ? 'tag-edit__linked-field__link--junction' : 'tag-edit__linked-field__link--line'
+      },
+      slug: {
+        lock: this.state.slugLocked ? 'tag-edit__linked-field__lock' : 'tag-edit__linked-field__lock--unlocked',
+        link: this.state.slugLocked ? 'tag-edit__linked-field__link--corner' : 'tag-edit__linked-field__link'
+      }
+    };
+
     return (
       <div className="tag-edit__input-group">
         <div className="tag-edit__name">
           <label>Internal Name</label>
           <input className="tag-edit__input" type="text" value={this.props.tag.internalName} onChange={this.onUpdateInternalName.bind(this)}/>
           <div className="tag-edit__linked-field">
-            <div className="tag-edit__linked-field__link--junction"></div>
-            <div className="tag-edit__linked-field__lock"></div>
+            <div className={classNames.externalName.link}></div>
+            <div className={classNames.externalName.lock} onClick={this.toggleUnlock.bind(this, 'externalNameLocked')}></div>
             <label>External Name</label>
-            <input type="text" disabled="true" value={this.props.tag.externalName} onChange={this.onUpdateExternalName.bind(this)}/>
+            <input type="text" value={this.props.tag.externalName} onChange={this.onUpdateExternalName.bind(this)}/>
+          </div>
+          <div className="tag-edit__linked-field">
+            <div className={classNames.slug.link}></div>
+            <div className={classNames.slug.lock} onClick={this.toggleUnlock.bind(this, 'slugLocked')}></div>
+            <label>Slug</label>
+            <input type="text" value={this.props.tag.slug} onChange={this.onUpdateSlug.bind(this)}/>
           </div>
         </div>
       </div>
