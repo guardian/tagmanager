8a30831015e025230cf541753cff3eef0615b94e
Christopher Lloyd <christopher.lloyd@theguardian.com>
2015-11-03 11:22:02 +0000

    Added fancy name change from composer

--- public/components/Tag/TagBasicInfo.react.js
+++ public/components/Tag/TagBasicInfo.react.js
@@ -38,14 +38,42 @@ export default class TagBasicInfo extends React.Component {
     }));
   }
 
-  toggleUnlock(name) {
-    var newState = {};
-    newState[name] = !this.state[name];
-    this.setState(newState);
+  toggleExternalNameLock() {
+
+    var newLockState = !this.state.externalNameLocked;
+
+    this.setState({
+      externalNameLocked: newLockState
+    });
+
+    if (newLockState) {
+      this.props.updateTag(Object.assign({}, this.props.tag, {
+        externalName: this.props.tag.internalName
+      }));
+    }
+  }
+
+  toggleSlugLock() {
+
+    var newLockState = !this.state.slugLocked;
+
+    this.setState({
+      slugLocked: newLockState
+    });
+
+    if (newLockState) {
+      this.props.updateTag(Object.assign({}, this.props.tag, {
+        slug: this.slugify(this.props.tag.internalName)
+      }));
+    }
   }
 
   slugify(text) {
     return text.toLowerCase().replace(/[^a-z0-9-]/g, '-');
+  }
+
+  getPathWithoutSlug() {
+    return this.props.tag.path.substring(0, this.props.tag.path.lastIndexOf('/') + 1);
   }
 
   render () {
@@ -65,6 +93,10 @@ export default class TagBasicInfo extends React.Component {
       }
     };
 
+    if (!this.state.slugLocked) {
+      classNames.externalName.link = this.state.externalNameLocked ? 'tag-edit__linked-field__link--corner' : 'tag-edit__linked-field__link';
+    }
+
     return (
       <div className="tag-edit__input-group">
         <div className="tag-edit__name">
@@ -72,15 +104,20 @@ export default class TagBasicInfo extends React.Component {
           <input className="tag-edit__input" type="text" value={this.props.tag.internalName} onChange={this.onUpdateInternalName.bind(this)}/>
           <div className="tag-edit__linked-field">
             <div className={classNames.externalName.link}></div>
-            <div className={classNames.externalName.lock} onClick={this.toggleUnlock.bind(this, 'externalNameLocked')}></div>
+            <div className={classNames.externalName.lock} onClick={this.toggleExternalNameLock.bind(this)}></div>
             <label>External Name</label>
-            <input type="text" value={this.props.tag.externalName} onChange={this.onUpdateExternalName.bind(this)}/>
+            <div className="tag-edit__linked-field__input-container">
+              <input type="text" value={this.props.tag.externalName} onChange={this.onUpdateExternalName.bind(this)} />
+            </div>
           </div>
           <div className="tag-edit__linked-field">
             <div className={classNames.slug.link}></div>
-            <div className={classNames.slug.lock} onClick={this.toggleUnlock.bind(this, 'slugLocked')}></div>
+            <div className={classNames.slug.lock} onClick={this.toggleSlugLock.bind(this)}></div>
             <label>Slug</label>
-            <input type="text" value={this.props.tag.slug} onChange={this.onUpdateSlug.bind(this)}/>
+            <div className="tag-edit__linked-field__input-container">
+              <span>{this.getPathWithoutSlug()}</span>
+              <input type="text" value={this.props.tag.slug} onChange={this.onUpdateSlug.bind(this)}/>
+            </div>
           </div>
         </div>
       </div>
