27761e6e398c086b6043a455f627c174b661643a
Christopher Lloyd <christopher.lloyd@theguardian.com>
2015-12-04 16:55:25 +0000

    Correct AuthAction and removed duplicate code

--- app/controllers/Support.scala
+++ app/controllers/Support.scala
@@ -2,21 +2,13 @@ package controllers
 
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
-import play.api.Logger
 import services.ImageMetadataService
 
 object Support extends Controller with PanDomainAuthActions {
 
-  def imageMetadata(imageUrl: String = "") = AuthAction {
-
-    val imageMetadata = ImageMetadataService.fetch(imageUrl)
-    Ok(Json.toJson(imageMetadata))
-
+  def imageMetadata(imageUrl: String = "") = APIAuthAction {
     ImageMetadataService.fetch(imageUrl).map { imageMetadata =>
       Ok(Json.toJson(imageMetadata))
     }.getOrElse(NotFound)
-
   }
-
-
 }
