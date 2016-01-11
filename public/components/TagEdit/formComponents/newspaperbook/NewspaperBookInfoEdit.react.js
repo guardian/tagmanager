0cabcc2d3ea4089c0a77a686f5945164a88d46e7
Christopher Lloyd <christopher.lloyd@theguardian.com>
2016-01-11 15:55:31 +0000

    Added Publication Select

---
+++ public/components/TagEdit/formComponents/newspaperbook/NewspaperBookInfoEdit.react.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import R from 'ramda';
+
+import PublicationSelect from '../../../utils/PublicationSelect.react';
+
+export default class ContributorInfoEdit extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  updatePublicationId(e) {
+    this.props.updateTag(Object.assign({}, this.props.tag, {
+      publication: parseInt(e.target.value, 10)
+    }));
+  }
+
+  render () {
+
+    return (
+      <div className="tag-edit__input-group">
+        <label className="tag-edit__input-group__header">Newspaper Book Information</label>
+        <div className="tag-edit__field">
+          <label className="tag-edit__label">Publication</label>
+          <PublicationSelect selectedId={this.props.tag.publication} onChange={this.updatePublicationId.bind(this)}/>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ public/components/utils/PublicationSelect.react.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import tagManagerApi from '../../util/tagManagerApi';
+
+export default class SectionSelect extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      publications: []
+    }
+  }
+
+  fetchPublications() {
+    tagManagerApi.searchTags('', {
+      tagType: 'Publication'
+    })
+    .then((tags) => {
+      console.log(tags)
+      this.setState({
+        publications: tags
+      });
+    });
+  }
+
+  componentDidMount() {
+    if (!this.state.publications || !this.state.publications.length) {
+      this.fetchPublications();
+    }
+  }
+
+  render () {
+
+    if (!this.state.publications || !this.state.publications.length) {
+      return (
+        <select disabled="true">
+          <option>Fetching publication names...</option>
+        </select>
+      );
+    }
+
+    return (
+      <select value={this.props.selectedId} onChange={this.props.onChange}>
+        {!this.props.selectedId || this.props.showBlank ? <option value={false}></option> : false}
+        {this.state.publications.sort((a, b) => {return a.internalName > b.internalName ? 1 : -1;}).map(function(s) {
+          return (
+            <option value={s.id} key={s.id} >{s.internalName}</option>
+          );
+        })}
+      </select>
+    );
+  }
+}
