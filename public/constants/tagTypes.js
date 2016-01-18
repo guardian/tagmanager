ef91cee7de40fd347dae420486f4418451d03251
Christopher Lloyd <christopher.lloyd@theguardian.com>
2016-01-18 13:27:43 +0000

    Tag Select Usability fixes

--- public/constants/tagTypes.js
+++ public/constants/tagTypes.js
@@ -1,5 +1,39 @@
-export const topic = 'Topic';
-export const series = 'Series';
-export const contributor = 'Contributor';
-export const publication = 'Publication';
-export const newspaperBook = 'NewspaperBook';
+export const topic = {
+  name: 'Topic',
+  displayName: 'Topic'
+};
+
+export const series = {
+  name: 'Series',
+  displayName: 'Series'
+};
+
+export const contributor = {
+  name: 'Contributor',
+  displayName: 'Contributor'
+};
+
+export const tone = {
+  name: 'Tone',
+  displayName: 'Tone'
+};
+
+export const contentType = {
+  name: 'ContentType',
+  displayName: 'Content Type'
+};
+
+export const publication = {
+  name: 'Publication',
+  displayName: 'Publication'
+};
+
+export const newspaperBook = {
+  name: 'NewspaperBook',
+  displayName: 'Newspaper Book'
+};
+
+export const newspaperBookSection = {
+  name: 'NewspaperBookSection',
+  displayName: 'Newspaper Book Section'
+};
