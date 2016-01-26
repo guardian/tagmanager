e2103244737d72917acd5337e9e5689d39359c33
hmgibson23 <hugo.gibson@yahoo.co.uk>
2016-01-26 10:46:01 +0000

    Add the synchroniser to the cluster

--- app/modules/clustersync/ClusterSynchronisation.scala
+++ app/modules/clustersync/ClusterSynchronisation.scala
@@ -23,6 +23,9 @@ class ClusterSynchronisation @Inject() (lifecycle: ApplicationLifecycle) {
 
   val reservation: AtomicReference[Option[NodeStatus]] = new AtomicReference[Option[NodeStatus]](None)
   val tagCacheSynchroniser: AtomicReference[Option[KinesisConsumer]] = new AtomicReference[Option[KinesisConsumer]](None)
+  val sectionCacheSynchroniser: AtomicReference[Option[KinesisConsumer]] = new
+    AtomicReference[Option[KinesisConsumer]](None)
+
 
   lifecycle.addStopHook{ () => Future.successful(stop) }
   serviceManager.startAsync()
@@ -42,9 +45,15 @@ class ClusterSynchronisation @Inject() (lifecycle: ApplicationLifecycle) {
       SectionLookupCache.refresh
 
       val tagUpdateConsumer = new KinesisConsumer(Config().tagUpdateStreamName, s"tag-cache-syncroniser-${ns.nodeId}", TagSyncUpdateProcessor)
-      Logger.info("starting sync consumer")
+      Logger.info("starting tag sync consumer")
       tagUpdateConsumer.start()
       tagCacheSynchroniser.set(Some(tagUpdateConsumer))
+
+      val sectionUpdateConsumer = new KinesisConsumer(Config().sectionUpdateStreamName,
+      s"section-cache-synchroniser-${ns.nodeId}", SectionSyncUpdateProcessor)
+      Logger.info("stating section sync consumer")
+      sectionUpdateConsumer.start()
+      sectionCacheSynchroniser.set(Some(sectionUpdateConsumer))
     } catch {
       case he: HeartbeatException => Logger.error("failed to register in the cluster, will try again next heartbeat")
       case NonFatal(e) => {
