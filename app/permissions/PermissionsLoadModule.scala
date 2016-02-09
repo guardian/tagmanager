44b90aacbb2776e6915ce93331f726d15c73913b
Sam Cutler <sam.cutler@guardian.co.uk>
2016-02-09 17:28:28 +0000

    Added permissions workaround

---
+++ app/permissions/PermissionsLoadModule.scala
@@ -0,0 +1,27 @@
+package modules.permissions
+
+import com.google.inject.AbstractModule
+
+import play.api.inject.ApplicationLifecycle
+import javax.inject._
+
+// This is requried because there's an issue in the permissions client
+// which causes the permissions to come back as denied for the first person
+// to request them. Seems to be a timing bug...
+//
+// This should only be a temporary fix @ 2016/02/09
+
+class PermissionsLoadModule extends AbstractModule {
+  override def configure(): Unit = {
+    bind(classOf[PermissionsLoader]).asEagerSingleton()
+  }
+}
+
+@Singleton
+class PermissionsLoader @Inject() (lifecycle: ApplicationLifecycle) {
+  load
+
+  def load {
+    Permissions.list(PermissionsUser("preload@permissions"))
+  }
+}
