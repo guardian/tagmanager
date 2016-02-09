d3f7ac7aea9e8a7a602c71360fff510d22bfaa34
Sam Cutler <sam.cutler@guardian.co.uk>
2016-02-09 17:39:51 +0000

    Added missing imports

--- app/permissions/PermissionsLoadModule.scala
+++ app/permissions/PermissionsLoadModule.scala
@@ -5,6 +5,9 @@ import com.google.inject.AbstractModule
 import play.api.inject.ApplicationLifecycle
 import javax.inject._
 
+import com.gu.editorial.permissions.client._
+import permissions._
+
 // This is requried because there's an issue in the permissions client
 // which causes the permissions to come back as denied for the first person
 // to request them. Seems to be a timing bug...
