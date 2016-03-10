22c0e781014392ed588ad6027e54b4b61526cb0b
Stephen Wells <stephen.wells@guardian.co.uk>
2016-03-10 16:28:24 +0000

    sponsorships now get removed from tags and sections correctly

--- app/repositories/SponsorshipOperations.scala
+++ app/repositories/SponsorshipOperations.scala
@@ -3,11 +3,12 @@ package repositories
 import com.gu.tagmanagement.{SectionEvent, EventType, TagEvent}
 import model.{SectionAudit, TagAudit}
 import services.KinesisStreams
+import play.api.Logger
 
 
 object SponsorshipOperations {
   def addSponsorshipToTag(sponsorshipId: Long, tagId: Long)(implicit username: Option[String]): Unit = {
-
+    Logger.info(s"adding sponsorship $sponsorshipId to tag $tagId")
     TagRepository.getTag(tagId).foreach { t =>
       val sponsoredTag = t.copy(activeSponsorships = sponsorshipId :: t.activeSponsorships )
       val result = TagRepository.upsertTag(sponsoredTag)
@@ -18,9 +19,9 @@ object SponsorshipOperations {
   }
 
   def removeSponsorshipFromTag(sponsorshipId: Long, tagId: Long)(implicit username: Option[String]): Unit = {
-
+    Logger.info(s"removing sponsorship $sponsorshipId from tag $tagId")
     TagRepository.getTag(tagId).foreach { t =>
-      val sponsoredTag = t.copy(activeSponsorships = t.activeSponsorships.filterNot(_ == tagId))
+      val sponsoredTag = t.copy(activeSponsorships = t.activeSponsorships.filterNot(_ == sponsorshipId))
       val result = TagRepository.upsertTag(sponsoredTag)
 
       KinesisStreams.tagUpdateStream.publishUpdate(sponsoredTag.id.toString, TagEvent(EventType.Update, sponsoredTag.id, Some(sponsoredTag.asThrift)))
@@ -29,7 +30,7 @@ object SponsorshipOperations {
   }
 
   def addSponsorshipToSection(sponsorshipId: Long, sectionId: Long)(implicit username: Option[String]): Unit = {
-
+    Logger.info(s"adding sponsorship $sponsorshipId to section $sectionId")
     SectionRepository.getSection(sectionId).foreach { s =>
       val sponsoredSection = s.copy(activeSponsorships = sponsorshipId :: s.activeSponsorships )
       val result = SectionRepository.updateSection(sponsoredSection)
@@ -41,9 +42,9 @@ object SponsorshipOperations {
   }
 
   def removeSponsorshipFromSection(sponsorshipId: Long, sectionId: Long)(implicit username: Option[String]): Unit = {
-
+    Logger.info(s"removing sponsorship $sponsorshipId from section $sectionId")
     SectionRepository.getSection(sectionId).foreach { s =>
-      val sponsoredSection = s.copy(activeSponsorships =  s.activeSponsorships.filterNot(_ == sectionId) )
+      val sponsoredSection = s.copy(activeSponsorships =  s.activeSponsorships.filterNot(_ == sponsorshipId) )
       val result = SectionRepository.updateSection(sponsoredSection)
 
       KinesisStreams.sectionUpdateStream.publishUpdate(sponsoredSection.id.toString, SectionEvent(EventType.Update, sponsoredSection.id, Some(sponsoredSection.asThrift)))
