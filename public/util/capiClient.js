a099a5be950f8a7dd25fb68eccf94353854592d0
Michael Barton <michael.barton@guardian.co.uk>
2017-01-05 15:47:18 +0000

    Do not break apart quoted strings when searching CAPI

--- public/util/capiClient.js
+++ public/util/capiClient.js
@@ -24,6 +24,47 @@ function getCapiPreviewUrl() {
   return store.getState().config.capiPreviewUrl + '/search?api-key=' + store.getState().config.capiKey;
 }
 
+// Ensure quoted strings are not split up into separate search tokens. For example:
+//   buildSearch('') === '';
+//   buildSearch('abc') === 'abc';
+//   buildSearch('abc def xyz') === 'abc AND def AND xyz';
+//   buildSearch('abc   def') === 'abc AND def';
+//   buildSearch('"abc def" xyz') === '"abc def" AND xyz';
+//   buildSearch('"abc def" xyz "beep   beep" bloop') === '"abc def" AND xyz AND "beep   beep" AND bloop';
+
+function buildSearch(searchString) {
+    const tokens = [];
+
+    let token = '';
+    let withinQuote = false;
+
+    for(const char of searchString) {
+        if(char === '"') {
+            withinQuote = !withinQuote;
+        }
+
+        if(withinQuote) {
+            token += char;
+        } else {
+            if(char === ' ') {
+                if(token != '') {
+                    tokens.push(token);
+                    token = '';
+                }
+            } else {
+                token += char;
+            }
+        }
+    }
+
+    // grab the last one
+    if(token != '') {
+        tokens.push(token);
+    }
+
+    return tokens.join(' AND ');
+}
+
 export function getByTag (tag, params) {
     const query = paramsObjectToQuery(params);
 
@@ -43,7 +84,7 @@ export function searchContent (searchString, params) {
     const searchQueryString = searchString || '';
 
     return Reqwest({
-      url: getCapiUrl() + '&q=' + searchQueryString.replace(' ', ' AND ')  + '&' + query,
+      url: getCapiUrl() + '&q=' + buildSearch(searchQueryString) + '&' + query,
       contentType: 'application/json',
       crossOrigin: true,
       method: 'get'
@@ -56,7 +97,7 @@ export function searchPreviewContent (searchString, params) {
     const searchQueryString = searchString || '';
 
     return Reqwest({
-      url: getCapiPreviewUrl() + '&q=' + searchQueryString.replace(' ', ' AND ')  + '&' + query + '&show-fields=isLive,internalComposerCode&order-by=newest',
+      url: getCapiPreviewUrl() + '&q=' + buildSearch(searchQueryString) + '&' + query + '&show-fields=isLive,internalComposerCode&order-by=newest',
       contentType: 'application/json',
       crossOrigin: true,
       method: 'get'
