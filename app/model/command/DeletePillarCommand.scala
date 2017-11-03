326d3d52252186f229ea9cb832cfda8fd716cb00
tomrf1 <tom.forbes@theguardian.com>
2017-11-03 11:49:43 +0000

    Remove path after delete

--- app/model/command/DeletePillarCommand.scala
+++ app/model/command/DeletePillarCommand.scala
@@ -11,9 +11,14 @@ case class DeletePillarCommand(id: Long) extends Command {
   override type T = Long
 
   override def process()(implicit username: Option[String] = None): Future[Option[Long]] = Future {
-    KinesisStreams.pillarUpdateStream.publishUpdate(id.toString, PillarEvent(PillarEventType.Delete, id, None))
-
-    PillarRepository.deletePillar(id)
+    for {
+      pillar <- PillarRepository.getPillar(id)
+      _ <- PillarRepository.deletePillar(id)
+    } yield {
+      KinesisStreams.pillarUpdateStream.publishUpdate(id.toString, PillarEvent(PillarEventType.Delete, id, None))
+      PathManager.removePathForId(pillar.pageId)
+      id
+    }
 
   }(Contexts.tagOperationContext)
 }
