44f1c455f71b6b8b4b1e13a99e65f38bf1b28e1d
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2019-09-09 14:44:43 +0100

    assert about the cache state before insertion in TagLookupCacheTest

--- test/repositories/TagLookupCacheTest.scala
+++ test/repositories/TagLookupCacheTest.scala
@@ -7,43 +7,43 @@ class TagLookupCacheTest extends FlatSpec with BeforeAndAfterEach with Matchers 
 
   private val cache = TagLookupCache
 
+  private val testTagId = 1
+
   override def beforeEach(): Unit = cache.allTags.set(Nil)
 
   behavior of "TagLookupCache.insertTag()"
 
   it should "insert new tag to cache" in {
-
     val testTag = createTestTag("test", 1, 0)
+    cache.allTags.get shouldEqual Nil
     cache.insertTag(testTag)
 
     cache.allTags.get.contains(testTag) shouldEqual true
   }
 
   it should "update the tag if the cache have older version of tag" in {
-    val tagId = 1
-    val testTag = createTestTag("test", tagId, 0)
+    val testTag = createTestTag("test", testTagId, 0)
+    cache.allTags.get shouldEqual Nil
 
     cache.insertTag(testTag)
+    cache.allTags.get.find(_.id == testTagId).get shouldEqual testTag
 
-    cache.allTags.get.find(_.id == tagId).get shouldEqual testTag
-
-    val testTagAfterUpdate = createTestTag("test updated", tagId, 1)
-
+    val testTagAfterUpdate = createTestTag("test updated", testTagId, 1)
     cache.insertTag(testTagAfterUpdate)
     cache.allTags.get.size shouldEqual 1
-    cache.allTags.get.find(_.id == tagId).get shouldEqual testTagAfterUpdate
+    cache.allTags.get.find(_.id == testTagId).get shouldEqual testTagAfterUpdate
   }
 
   it should "do not update the tag if the cache have already more up to date tag version" in {
-    val tagId = 1
-    val mostRecentTag = createTestTag("test", tagId, 1)
+    cache.allTags.get shouldEqual Nil
+    val mostRecentTag = createTestTag("test", testTagId, 1)
     cache.insertTag(mostRecentTag)
 
-    val notUpToDateTestTagForUpdateRequestWith = createTestTag("test updated", tagId, 0)
+    val notUpToDateTestTagForUpdateRequestWith = createTestTag("test updated", testTagId, 0)
 
     cache.insertTag(notUpToDateTestTagForUpdateRequestWith)
     cache.allTags.get.size shouldEqual 1
-    cache.allTags.get.find(_.id == tagId).get shouldEqual mostRecentTag
+    cache.allTags.get.find(_.id == testTagId).get shouldEqual mostRecentTag
   }
 
 }
