bf68c4b0dca0b95d22cef55032ff46b14116a8fa
Andrew Nowak <andrew.nowak@guardian.co.uk>
2024-06-13 16:23:02 +0100

    Logging of permission status

--- app/controllers/PanDomainAuthActions.scala
+++ app/controllers/PanDomainAuthActions.scala
@@ -1,13 +1,26 @@
 package controllers
 
 import com.gu.pandahmac.HMACAuthActions
+import com.gu.pandomainauth.PanDomain
 import com.gu.pandomainauth.model.AuthenticatedUser
 import services.Config
+import permissions.Permissions
+import play.api.Logging
 
-trait PanDomainAuthActions extends HMACAuthActions {
+trait PanDomainAuthActions extends HMACAuthActions with Logging {
 
   override def validateUser(authedUser: AuthenticatedUser): Boolean = {
-    (authedUser.user.email endsWith ("@guardian.co.uk")) && authedUser.multiFactor
+    val isValid = PanDomain.guardianValidation(authedUser)
+
+    val canAccess = Permissions.testUser(Permissions.TagManagerAccess)(authedUser.user.email)
+
+    if (!isValid) {
+      logger.warn(s"User ${authedUser.user.email} is not valid")
+    } else if (!canAccess) {
+      logger.warn(s"User ${authedUser.user.email} does not have tag_manager_access permission")
+    }
+
+    isValid // TODO && canAccess
   }
 
   override def cacheValidation = true
