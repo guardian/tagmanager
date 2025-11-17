5617dee8384b13e00bd3b503adb0df7e2901427e
Anna Beddow <anna.beddow@gmail.com>
2025-11-17 14:55:43 +0000

    Remove duplicate keyword selection field

--- public/components/Tag/Display.react.js
+++ public/components/Tag/Display.react.js
@@ -117,10 +117,6 @@ class TagDisplay extends React.Component {
                 <label className="tag-edit__input-group__header">Tag Type</label>
                 <TypeSelect selectedType={this.props.tag.type} types={this.props.config.tagTypes} forceDisabled={true}/>
               </div>
-                <div className="tag-edit__input-group">
-                    <label className="tag-edit__input-group__header">Keyword Type</label>
-                    <TypeSelect selectedType={this.props.tag.keywords} types={this.props.config.keywordTypes} forceDisabled={false}/>
-                </div>
               <TagEdit tag={this.props.tag} sections={this.props.sections} updateTag={this.props.tagActions.updateTag} pathLocked={true} tagEditable={this.props.tagEditable}/>
               <TagValidationErrors validations={validateTag(this.props.tag)} />
               {this.renderDeleteButton()}
