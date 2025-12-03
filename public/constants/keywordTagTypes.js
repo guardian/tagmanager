c9cfb8585e52ee51869aaa321412444498b706f9
Simon Byford <simon.byford@guardian.co.uk>
2025-12-03 17:36:41 +0000

    Reorder so "Other" is at the bottom

--- public/constants/keywordTagTypes.js
+++ public/constants/keywordTagTypes.js
@@ -2,7 +2,7 @@ export const keywordTagTypes = [
     {value: 'PERSON', label: 'Person'},
     {value: 'ORGANISATION', label: 'Organisation'},
     {value: 'EVENT', label: 'Event'},
+    {value: 'WORK_OF_ART_OR_PRODUCT', label: 'Work of art of product'},
     {value: 'PLACE', label: 'Place'},
-    {value: 'OTHER', label: 'Other'},
-    {value: 'WORK_OF_ART_OR_PRODUCT', label: 'Work of art of product'}
+    {value: 'OTHER', label: 'Other'}
 ];
