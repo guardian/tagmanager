877330c1fa4c8b6cd5a64171bc254b663e80f3f5
Charlotte <charlotte.emms@theguardian.com>
2026-09-03 16:28:24 +0100

    install snapshot version of tags thrift schema, update commercial information model

--- app/model/CommercialInformation.scala
+++ app/model/CommercialInformation.scala
@@ -4,9 +4,8 @@ import play.api.libs.json._
 import ai.x.play.json.Jsonx
 import ai.x.play.json.Encoders.encoder
 import com.gu.tagmanagement.{CommercialInformation => ThriftCommercialInformation, IABTaxonomyInformation => ThriftIABTaxonomyInformation}
-import repositories.Sequences.tagId
 
-case class IabTaxonomyInformation(tagId: String, taxonomyCode: String, modelId: Option[String]) {
+case class IABTaxonomyInformation(tagId: String, taxonomyCode: String, modelId: Option[String]) {
   def asThrift = ThriftIABTaxonomyInformation(
     tagId = tagId,
     taxonomyCode = taxonomyCode,
@@ -20,37 +19,36 @@ case class IabTaxonomyInformation(tagId: String, taxonomyCode: String, modelId: 
   }
 }
 
-object IabTaxonomyInformation {
-  implicit val format: OFormat[IabTaxonomyInformation] = Jsonx.formatCaseClass[IabTaxonomyInformation]
+object IABTaxonomyInformation {
+  implicit val iabTaxonomyFormat: OFormat[IABTaxonomyInformation] = Jsonx.formatCaseClass[IABTaxonomyInformation]
 
-  def apply(thriftIABTaxonomyInformation: ThriftIABTaxonomyInformation): IabTaxonomyInformation =
-    IabTaxonomyInformation(
-      tagId=thriftIABTaxonomyInformation.tagId, 
+  def apply(thriftIABTaxonomyInformation: ThriftIABTaxonomyInformation): IABTaxonomyInformation =
+    IABTaxonomyInformation(
+      tagId = thriftIABTaxonomyInformation.tagId,
       taxonomyCode = thriftIABTaxonomyInformation.taxonomyCode, 
       modelId = thriftIABTaxonomyInformation.modelId
-      )
+    )
 }
 
-case class CommercialInformation(commercialType: String, iabTaxonomyInformation: Option[IabTaxonomyInformation]) {
-
+case class CommercialInformation(commercialType: String, iabTaxonomyInformation: Option[IABTaxonomyInformation]) {
   def asThrift = ThriftCommercialInformation(
       commercialType = commercialType,
-      // iabTaxonomyInformation = iabTaxonomyInformation.map(t=>t.asThrift)
-
+      iabTaxonomyInformation = iabTaxonomyInformation.map(_.asThrift)
   )
 
   def asExportedXml = {
     <commercialType>{this.commercialType}</commercialType>
+    <iabTaxonomy>{this.iabTaxonomyInformation}</iabTaxonomy>
   }
 }
 
 object CommercialInformation {
 
-  implicit val trackingFormat: OFormat[CommercialInformation] = Jsonx.formatCaseClass[CommercialInformation]
+  implicit val commercialFormat: OFormat[CommercialInformation] = Jsonx.formatCaseClass[CommercialInformation]
 
   def apply(thriftCommercialInformation: ThriftCommercialInformation): CommercialInformation =
     CommercialInformation(
       commercialType = thriftCommercialInformation.commercialType,
-      iabTaxonomyInformation = thriftCommercialInformation.iabTaxonomyInformation
+      iabTaxonomyInformation = thriftCommercialInformation.iabTaxonomyInformation.map(IABTaxonomyInformation(_))
     )
 }
